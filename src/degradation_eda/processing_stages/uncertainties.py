aeb3500964aec892c062337331970d7a37a237c9
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-04-08 14:25:26 +0100

    Use zero uncertainty for masked pixels

--- src/degradation_eda/processing_stages/uncertainties.py
+++ src/degradation_eda/processing_stages/uncertainties.py
@@ -1,6 +1,7 @@
 from typing import Any, TypeVar
 
-from numpy import dtype, empty_like, maximum, ndarray, number, sqrt
+from numpy import bool_, broadcast_to, dtype, empty_like, maximum, ndarray, number, sqrt
+from numpy.ma import masked_where
 
 from degradation_eda.utils.uncertain_maths import Uncertain, uncertain
 
@@ -8,7 +9,8 @@ FramesShape = TypeVar("FramesShape", bound=Any)
 
 
 def estimate_poisson_uncertainty(
-    frames: ndarray[FramesShape, dtype[number]]
+    frames: ndarray[FramesShape, dtype[number]],
+    mask: ndarray[Any, dtype[bool_]],
 ) -> ndarray[FramesShape, Uncertain]:
     """Estimates the poisson uncertainty of photon counts.
 
@@ -18,6 +20,7 @@ def estimate_poisson_uncertainty(
     Args:
         frames (ndarray[FramesShape, dtype[number]]): A stack of frames from which the
             uncertainties can be computed.
+        mask (ndarray[Any, dtype[bool_]]): The boolean mask to apply to each frame.
 
     Returns:
         ndarray[FramesShape, Uncertain]: A stack of frames and their pixel-wise
@@ -25,5 +28,7 @@ def estimate_poisson_uncertainty(
     """
     results = empty_like(frames, dtype=uncertain)
     results["nominal"] = frames
-    results["uncertainty"] = maximum(sqrt(frames), 1.0)
+    results["uncertainty"] = masked_where(
+        broadcast_to(mask, frames.shape), maximum(sqrt(frames), 1.0)
+    ).filled(fill_value=0)
     return results
