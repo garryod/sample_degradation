793bd85c09f107113ec82f51b94300f2a971e913
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-04-13 10:04:36 +0100

    Add self absorbtion correction

--- src/degradation_eda/processing_stages/__init__.py
+++ src/degradation_eda/processing_stages/__init__.py
@@ -4,6 +4,7 @@ from .flux_and_transmission import correct_flux_and_transmission
 from .frame_time import correct_frame_time
 from .masking import mask_frames
 from .reading import load_data
+from .self_absorbtion import correct_self_absorbtion
 from .uncertainties import estimate_poisson_uncertainty
 
 __all__ = [
@@ -14,4 +15,5 @@ __all__ = [
     "correct_dark_current",
     "correct_frame_time",
     "correct_flux_and_transmission",
+    "correct_self_absorbtion",
 ]
---
+++ src/degradation_eda/processing_stages/self_absorbtion.py
@@ -0,0 +1,116 @@
+from typing import Any, Tuple, TypeVar, cast
+
+from numpy import (
+    cos,
+    dtype,
+    empty,
+    floating,
+    hypot,
+    integer,
+    linspace,
+    log,
+    meshgrid,
+    ndarray,
+    power,
+    tan,
+)
+
+from degradation_eda.utils.uncertain_maths import (
+    Uncertain,
+    multiply_uncertain,
+    uncertain,
+)
+
+FramesShape = TypeVar("FramesShape", bound=Any)
+
+
+def pixel_angles(
+    frame_shape: Tuple[int, int],
+    beam_center: Tuple[float, float],
+    pixel_sizes: Tuple[float, float],
+    distance: float,
+) -> ndarray[Tuple[int, int], dtype[integer]]:
+    """Computes the angles of pixels from the sample for a given geometry.
+
+    Args:
+        frame_shape (Tuple[int, int]): The shape of a frame.
+        beam_center (Tuple[float, float]): The center position of the beam in pixels.
+        pixel_sizes (Tuple[float, float]): The real space size of a detector pixel.
+        distance (float): The distance between the detector and the sample.
+
+    Returns:
+        ndarray[Tuple[int, int], dtype[integer]]: An array of pixel angles from the
+            sample.
+    """
+    yy, xx = meshgrid(
+        linspace(
+            -beam_center[1] * pixel_sizes[1],
+            (frame_shape[1] - beam_center[1]) * pixel_sizes[1],
+            frame_shape[1],
+        ),
+        linspace(
+            -beam_center[0] * pixel_sizes[0],
+            (frame_shape[0] - beam_center[0]) * pixel_sizes[0],
+            frame_shape[0],
+        ),
+    )
+    return tan(hypot(xx, yy) / distance)
+
+
+def self_absorbtion_correction_factors(
+    frame_shape: Tuple[int, int],
+    beam_center: Tuple[float, float],
+    pixel_sizes: Tuple[float, float],
+    distance: float,
+    transmissibility: float,
+) -> ndarray[Any, dtype[floating]]:
+    """Computes the self absorbtion correction factors given geometry and transmissibility.
+
+    Args:
+        frame_shape (Tuple[int, int]): The shape of a frame.
+        beam_center (Tuple[float, float]): The center position of the beam in pixels.
+        pixel_sizes (Tuple[float, float]): The real space size of a detector pixel.
+        distance (float): The distance between the detector and the sample.
+        transmissibility (float): The transmissibility of the sample.
+
+    Returns:
+        ndarray[Any, dtype[floating]]: An array of correction factors to be applied to
+            frames.
+    """
+    angles = pixel_angles(frame_shape, beam_center, pixel_sizes, distance)
+    return (1 - power(transmissibility, 1 / cos(angles) - 1)) / (
+        log(transmissibility) * (1 - 1 / cos(angles))
+    )
+
+
+def correct_self_absorbtion(
+    frames: ndarray[FramesShape, Uncertain],
+    beam_center: Tuple[float, float],
+    pixel_sizes: Tuple[float, float],
+    distance: float,
+    transmissibility: float,
+) -> ndarray[FramesShape, Uncertain]:
+    """Correct for transmission loss due to differences in observation angle.
+
+    Args:
+        frames (ndarray[FramesShape, Uncertain]): A stack of uncertain frames to be
+            corrected.
+        beam_center (Tuple[float, float]): The center position of the beam in pixels.
+        pixel_sizes (Tuple[float, float]): The real space size of a detector pixel.
+        distance (float): The distance between the detector and the sample.
+        transmissibility (float): The transmissibility of the sample.
+
+    Returns:
+        ndarray[FramesShape, Uncertain]: The corrected stack of frames and their
+            updated uncertainties.
+    """
+    correction_factors = empty(frames.shape[1:], uncertain)
+    correction_factors["nominal"] = self_absorbtion_correction_factors(
+        cast(Tuple[int, int], frames.shape[1:]),
+        beam_center,
+        pixel_sizes,
+        distance,
+        transmissibility,
+    )
+    correction_factors["uncertainty"] = 0
+    return multiply_uncertain(frames, correction_factors)
