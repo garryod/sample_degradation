9c8e51964b20ba2a8ce857f838d776e5586a4cf7
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-04-19 16:32:11 +0100

    Add slice option to load_data

--- src/degradation_eda/processing_stages/reading.py
+++ src/degradation_eda/processing_stages/reading.py
@@ -1,20 +1,23 @@
 from typing import Any, TypeVar
 
 from nexusformat.nexus import NXFile
-from numpy import array, dtype, ndarray, number
+from numpy import dtype, ndarray, number, s_
 
 LoadedDType = TypeVar("LoadedDType", bound=dtype[number])
 
 
-def load_data(path: str, key: str) -> ndarray[Any, LoadedDType]:
+def load_data(
+    path: str, key: str, use_slice: slice = s_[:]
+) -> ndarray[Any, LoadedDType]:
     """Read raw data at key from a nexus file at path.
 
     Args:
         path (str): The path to the nexus file.
         key (str): The key of the data within the nexus file.
+        use_slice (slice): The slice of data to load. Default all.
 
     Returns:
         ndarray[Any, LoadedDType]: A numpy array of data.
     """
     with NXFile(path, "r") as file:
-        return array(file[key])
+        return file[key][use_slice]
