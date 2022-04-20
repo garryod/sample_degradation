1940839bbf20ab8bd79300526790b9e3acb481f7
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-04-20 16:38:41 +0100

    Add MappedDataset frame iterator

--- src/degradation_eda/processing_stages/reading.py
+++ src/degradation_eda/processing_stages/reading.py
@@ -1,7 +1,8 @@
-from typing import Any, TypeVar
+from typing import Any, Tuple, TypeVar
 
+from h5py import Dataset
 from nexusformat.nexus import NXFile
-from numpy import dtype, ndarray, number, s_
+from numpy import dtype, ndarray, number, s_, zeros_like
 
 LoadedDType = TypeVar("LoadedDType", bound=dtype[number])
 
@@ -21,3 +22,34 @@ def load_data(
     """
     with NXFile(path, "r") as file:
         return file[key][use_slice]
+
+
+class MappedReader:
+    """Open a mapped dataset and provide an interator of frames."""
+
+    def __init__(self, path: str, dataset: str, frame_dims: int = 2):
+        self.frame_dims = frame_dims
+        self.dataset: Dataset = NXFile(path)[dataset]
+
+    def __iter__(self) -> "MappedReader":
+        self._index = zeros_like(self.dataset.shape[: -self.frame_dims])
+        return self
+
+    def __next__(self) -> ndarray[Tuple[int, ...], dtype[number]]:
+        frame = self[tuple(self._index)]
+        self._increment_index()
+        return frame
+
+    def __getitem__(
+        self, index: Tuple[int, ...]
+    ) -> ndarray[Tuple[int, ...], dtype[number]]:
+        return self.dataset[index]
+
+    def _increment_index(self):
+        self._index[-1] += 1
+        for pos in reversed(range(len(self._index))):
+            if self._index[pos] == self.dataset.shape[pos]:
+                if pos == 0:
+                    raise StopIteration
+                self._index[pos - 1] += 1
+                self._index[pos] = 0
