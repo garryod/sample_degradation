a83dedced7e746987693eaf8c27da3258adac510
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-04-25 10:49:30 +0100

    Add reference thickness normalization

--- src/degradation_eda/processing_stages/thickness.py
+++ src/degradation_eda/processing_stages/thickness.py
@@ -14,6 +14,10 @@ def normalize_thickness(
 ) -> MaskedArray[FramesShape, Uncertain]:
     """Normailizes pixel intensities by dividing by the sample thickness.
 
+    Normailizes pixel intensities by dividing by the sample thickness, as detailed in
+    section 3.4.3 of 'Everything SAXS: small-angle scattering pattern collection and
+    correction' [https://doi.org/10.1088/0953-8984/25/38/383201].
+
     Args:
         frames (MaskedArray[FramesShape, Uncertain]): A stack of uncertain frames to be
             corrected.
