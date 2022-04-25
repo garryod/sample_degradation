e7086664761538e4bce6c9a20e415b0a410f5aee
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-04-25 10:48:11 +0100

    Fix solid angle correction

--- src/degradation_eda/processing_stages/solid_angle.py
+++ src/degradation_eda/processing_stages/solid_angle.py
@@ -1,6 +1,6 @@
 from typing import Tuple, TypeVar
 
-from numpy import arccos, empty
+from numpy import arccos, empty, power
 from numpy.ma import MaskedArray, masked_array
 
 from degradation_eda.processing_stages.common import scattering_angles
@@ -15,7 +15,11 @@ def correct_solid_angle(
     pixel_sizes: Tuple[float, float],
     distance: float,
 ) -> MaskedArray[FramesShape, Uncertain]:
-    """Corrects for the solid angle subtended by each pixel.
+    """Corrects for the solid angle by scaling by the inverse of area subtended by a pixel.
+
+    Corrects for the solid angle by scaling by the inverse of area subtended by each
+    pixel, as detailed in section 3.4.6 of 'Everything SAXS: small-angle scattering
+    pattern collection and correction' [https://doi.org/10.1088/0953-8984/25/38/383201].
 
     Args:
         frames (MaskedArray[FramesShape, Uncertain]): A stack of uncertain frames to be
@@ -29,8 +33,9 @@ def correct_solid_angle(
             updated uncertainties.
     """
     correction = empty(frames.shape[1:], dtype=uncertain)
-    correction["nominal"] = arccos(
-        scattering_angles(frames.shape[1:], beam_center, pixel_sizes, distance)
+    correction["nominal"] = power(
+        arccos(scattering_angles(frames.shape[1:], beam_center, pixel_sizes, distance)),
+        3,
     )
     correction["uncertainty"] = 0
     return masked_array(
