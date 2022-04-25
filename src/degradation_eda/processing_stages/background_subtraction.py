d42ec71e4f7d9349d7866c21cccd19308f53dc78
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-04-25 09:44:59 +0100

    Add reference to background subtraction

--- src/degradation_eda/processing_stages/background_subtraction.py
+++ src/degradation_eda/processing_stages/background_subtraction.py
@@ -13,6 +13,10 @@ def subtract_background(
 ) -> MaskedArray[FramesShape, Uncertain]:
     """Subtract a background frame from a sequence of foreground frames.
 
+    Subtract a background frame from a sequence of foreground frames, as detailed in
+    section 3.4.6 of 'Everything SAXS: small-angle scattering pattern collection and
+    correction' [https://doi.org/10.1088/0953-8984/25/38/383201].
+
     Args:
         foreground_frames (MaskedArray[FramesShape, Uncertain]): A sequence of
             foreground frames to be corrected.
