60feb93127d51cbe7ad63a5c1a9bd0370125825b
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-04-25 09:17:29 +0100

    Fix frame time normalization

--- src/degradation_eda/processing_stages/frame_time.py
+++ src/degradation_eda/processing_stages/frame_time.py
@@ -1,15 +1,6 @@
 from typing import Any, TypeVar
 
-from numpy import (
-    add,
-    atleast_1d,
-    broadcast_shapes,
-    dtype,
-    empty,
-    expand_dims,
-    floating,
-    ndarray,
-)
+from numpy import atleast_1d, dtype, empty, expand_dims, floating, ndarray
 from numpy.ma import MaskedArray, masked_array
 
 from degradation_eda.utils.uncertain_maths import Uncertain, divide_uncertain, uncertain
@@ -21,31 +12,27 @@ TimesShape = TypeVar("TimesShape", bound=Any)
 def normalize_frame_time(
     frames: MaskedArray[FramesShape, Uncertain],
     count_times: ndarray[TimesShape, dtype[floating]],
-    dead_times: ndarray[TimesShape, dtype[floating]],
 ) -> MaskedArray[FramesShape, Uncertain]:
-    """Normalize for detector frame rate by scaling photon counts according to frame time.
+    """Normalize for detector frame rate by scaling photon counts according to count time.
+
+    Normalize for detector frame rate by scaling photon counts according to count time,
+    as detailed in section 3.4.3 of 'Everything SAXS: small-angle scattering pattern
+    collection and correction' [https://doi.org/10.1088/0953-8984/25/38/383201].
 
     Args:
         frames (MaskedArray[FramesShape, Uncertain]): A stack of frames to be
             normalized.
         count_times (ndarray[TimesShape, dtype[floating]]): The period over which
             photons are counted for each frame.
-        dead_times (ndarray[TimesShape, dtype[floating]]): The period over which
-            photons are not counted for each frame.
 
     Returns:
         MaskedArray[FramesShape, Uncertain]: The normalized stack of frames and their
             updated uncertainties.
     """
-    scale_factors = expand_dims(
-        atleast_1d(
-            empty(broadcast_shapes(count_times.shape, dead_times.shape), uncertain)
-        ),
-        (1, 2),
+    times = expand_dims(
+        atleast_1d(empty(count_times.shape, uncertain)),
+        (-2, -1),
     )
-    scale_factors["nominal"] = expand_dims(
-        atleast_1d(add(count_times, dead_times)),
-        (1, 2),
-    )
-    scale_factors["uncertainty"] = 0
-    return masked_array(divide_uncertain(frames.data, scale_factors), frames.mask)
+    times["nominal"] = expand_dims(atleast_1d(count_times), (-2, -1))
+    times["uncertainty"] = 0
+    return masked_array(divide_uncertain(frames.data, times), frames.mask)
